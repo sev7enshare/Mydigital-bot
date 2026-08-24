f2b16c73106dbf074d1c416c77f4292a02ff80c8
sev7en 分享 <sev7enshare@gmail.com>
2026-08-24 21:34:16 +0800

    Send ad learning candidates after startup

--- src/main/java/top/feiyangdigital/scheduledTasks/AdLearningCandidateScheduler.java
+++ src/main/java/top/feiyangdigital/scheduledTasks/AdLearningCandidateScheduler.java
@@ -1,6 +1,8 @@
 package top.feiyangdigital.scheduledTasks;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.bots.AbsSender;
@@ -23,6 +25,15 @@ public class AdLearningCandidateScheduler {
 
     @Scheduled(cron = "0 30 9 * * *", zone = "Asia/Shanghai")
     public void sendDailyAdLearningCandidates() {
+        sendCandidates();
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendStartupAdLearningCandidates() {
+        sendCandidates();
+    }
+
+    private void sendCandidates() {
         AbsSender sender = "webhook".equals(BaseInfo.getBotMode()) ? tgWebhookBot : tgLongPollingBot;
         adLearningRuleReviewService.sendDailyCandidates(sender);
     }
