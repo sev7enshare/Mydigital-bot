ebbd0784e0e7c453df1c3c7949383e9eb3a7d0b3
sev7en 分享 <sev7enshare@gmail.com>
2026-08-24 21:24:29 +0800

    Add daily ad learning candidate scheduler

---
+++ src/main/java/top/feiyangdigital/scheduledTasks/AdLearningCandidateScheduler.java
@@ -0,0 +1,29 @@
+package top.feiyangdigital.scheduledTasks;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+import top.feiyangdigital.bot.TgLongPollingBot;
+import top.feiyangdigital.bot.TgWebhookBot;
+import top.feiyangdigital.entity.BaseInfo;
+import top.feiyangdigital.handleService.AdLearningRuleReviewService;
+
+@Component
+public class AdLearningCandidateScheduler {
+
+    @Autowired
+    private TgLongPollingBot tgLongPollingBot;
+
+    @Autowired
+    private TgWebhookBot tgWebhookBot;
+
+    @Autowired
+    private AdLearningRuleReviewService adLearningRuleReviewService;
+
+    @Scheduled(cron = "0 30 9 * * *", zone = "Asia/Shanghai")
+    public void sendDailyAdLearningCandidates() {
+        AbsSender sender = "webhook".equals(BaseInfo.getBotMode()) ? tgWebhookBot : tgLongPollingBot;
+        adLearningRuleReviewService.sendDailyCandidates(sender);
+    }
+}
