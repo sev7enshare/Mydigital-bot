68439922795f28f26c9032e59c550c9278e66d3e
sev7en 分享 <sev7enshare@gmail.com>
2026-08-24 21:24:55 +0800

    Enable scheduled ad learning reviews

--- src/main/java/top/feiyangdigital/TgBotApplication.java
+++ src/main/java/top/feiyangdigital/TgBotApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -22,6 +23,7 @@ import java.util.List;
 
 @SpringBootApplication
 @EnableCaching
+@EnableScheduling
 @MapperScan("top.feiyangdigital.mapper")
 public class TgBotApplication implements CommandLineRunner {
 
